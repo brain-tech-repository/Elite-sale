c142e8547a980fd5b04334876d2ac80f7106bcea
brain-tech-repository <prakashnisad69@gmail.com>
2026-03-05 13:02:50 +0530

    first commit

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
+import { useRouter } from "next/navigation";
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Field,
@@ -18,6 +19,12 @@ export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const router = useRouter();
+
+const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  router.push("/salesDashboard");
+};
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
@@ -32,7 +39,7 @@ export function LoginForm({
               />
             </div>
           </div>
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8"  onSubmit={handleSubmit}>
             <FieldGroup>
               <div className="flex flex-col items-center gap-2 text-center">
                 <h1 className="text-2xl font-bold">Welcome back</h1>
